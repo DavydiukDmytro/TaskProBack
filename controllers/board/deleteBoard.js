734addb6ce16b1dd7e32883c6544d01ba3ab523e
NikitaDima <onekill13@gmail.com>
2023-08-15 01:09:07 +0300

    delete board

--- controllers/board/deleteBoard.js
+++ controllers/board/deleteBoard.js
@@ -1,23 +1,23 @@
 const { requestError } = require('../../helpers');
 const Board = require('../../models/board');
+const Column = require('../../models/column');
+const Task = require('../../models/task');
 
 const deleteBoard = async (req, res) => {
   const { boardId } = req.params;
-  const result = await Board.findByIdAndDelete(boardId);
-  if (!result) {
-    throw requestError(404, `Board ${boardId} not found`);
-  }
-  // const columns = await Column.find({ board: boardId }).populate('tasks');
-  // for (const column of columns) {
-  //   if (column.tasks.length) {
-  //     await Task.deleteMany({
-  //       _id: { $in: column.tasks.map(task => task._id) },
-  //     });
-  //   }
-  //   await Column.findByIdAndDelete(column.id);
-  // }
+  const board = await Board.findById(boardId);
 
-  res.status(204).json();
+  if (!board) {
+    throw requestError(404, 'Board not found');
+  }
+  const columns = await Column.find({ board: board._id });
+
+  for (const column of columns) {
+    await Task.deleteMany({ column: column._id });
+    await column.remove();
+  }
+  await board.remove();
+  res.status(200).json();
 };
 
 module.exports = deleteBoard;
--- controllers/column/deleteColumn.js
+++ controllers/column/deleteColumn.js
@@ -1,18 +1,17 @@
 const { requestError } = require('../../helpers');
 const Column = require('../../models/column');
+const Task = require('../../models/task'); 
 
 const deleteColumn = async (req, res) => {
   const { columnId } = req.params;
-  const column = await Column.findByIdAndDelete(columnId);
+  const column = await Column.findById(columnId);
   if (!column) {
-    throw requestError(404, `Column ${columnId} not found`);
+    throw requestError(404, 'Column not found');
   }
-
-  // const { tasks } = column;
-  // await Task.deleteMany({ _id: { $in: tasks.map(task => task._id) } });
-  // await Column.findByIdAndDelete(columnId);
-
-  res.status(204).json();
+  await Task.deleteMany({ column: column._id });
+  await column.remove();
+  res.status(200).json()
 };
 
 module.exports = deleteColumn;
+
